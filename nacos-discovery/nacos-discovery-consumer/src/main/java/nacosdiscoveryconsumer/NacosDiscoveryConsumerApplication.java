30687e40646fcccbe741f980c382377b6cf00123
vozrr <2744919353@qq.com>
2021-03-11 18:03:53 +0800

    服务注册与发现

---
+++ nacos-discovery/nacos-discovery-consumer/src/main/java/nacosdiscoveryconsumer/NacosDiscoveryConsumerApplication.java
@@ -0,0 +1,15 @@
+package nacosdiscoveryconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosDiscoveryConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacosDiscoveryConsumerApplication.class, args);
+	}
+
+}
---
+++ nacos-discovery/nacos-discovery-provider/src/test/java/top/vozrr/springcloud/nacosdiscoveryprovider/NacosDiscoveryProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package top.vozrr.springcloud.nacosdiscoveryprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosDiscoveryProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
